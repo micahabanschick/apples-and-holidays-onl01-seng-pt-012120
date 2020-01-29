8f23726f0b60c271fe328e188524520e8bd871ee
Micah Banschick <micha.ban@gmail.com>
2020-01-29 17:09:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,6 +41,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     }
   }
   holiday_hash[:winter][:christmas].push("Balloons")
+  #holiday_hash[:winter][:christmas].push("Balloons")
 end
 
 
